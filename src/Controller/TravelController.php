aaeaf0cea54dddd076fdc4f8a8bed24c54a1bb63
l00ma <looma@protonmail.ch>
2023-05-11 19:30:59 +0200

    weather icon added for destination.html.twig page

--- src/Controller/TravelController.php
+++ src/Controller/TravelController.php
@@ -60,11 +60,13 @@ class TravelController extends AbstractController
         $url = 'https://api.openweathermap.org/data/2.5/weather?lat=' . $travel['lat'] . '&lon=' . $travel['long'] . '&appid=' . $apikey . '&units=metric&lang=fr';
         $result = file_get_contents($url);
         $weatherResult = json_decode($result, true);
-        $icon = 'https://openweathermap.org/img/w/' . $weatherResult['weather']['0']['icon'] . '.png';
+        $temp = (int) $weatherResult['main']['temp'];
+
+        $icon = 'https://openweathermap.org/img/wn/' . $weatherResult['weather']['0']['icon'] . '@2x.png';
 
         return $this->twig->render('Travel/destination.html.twig', [
             'travel' => $travel,
-            'temp' => $weatherResult['main']['temp'],
+            'temp' => $temp,
             'weather' => $weatherResult['weather']['0']['description'],
             'icon' => $icon,
         ]);
