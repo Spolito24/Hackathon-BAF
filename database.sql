f97cf3f29dfe13fe42b27a453fcf3ba58bbea6a6
l00ma <looma@protonmail.ch>
2023-05-11 14:35:39 +0200

    travel index page construction

--- database.sql
+++ database.sql
@@ -1,63 +1,35 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Jeu 26 Octobre 2017 à 13:53
--- Version du serveur :  5.7.19-0ubuntu0.16.04.1
--- Version de PHP :  7.0.22-0ubuntu0.16.04.1
+-- Adminer 4.8.1 MySQL 8.0.32-0ubuntu0.22.04.2 dump
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+SET NAMES utf8mb4;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+DROP DATABASE IF EXISTS `hackathon1`;
+CREATE DATABASE `hackathon1` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `hackathon1`;
 
---
--- Base de données :  `simple-mvc`
---
+DROP TABLE IF EXISTS `destination`;
+CREATE TABLE `destination` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `country` varchar(150) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `lat` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `long` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
+INSERT INTO `destination` (`id`, `country`, `city`, `lat`, `long`) VALUES
+(1,	'France',	'Paris',	'48.8534',	'2.3488'),
+(2,	'USA',	'New-York',	'40.7143',	'-74.006'),
+(3,	'Japan',	'Tokyo',	'35.6895',	'139.6917'),
+(4,	'Canada',	'Toronto',	'43.7001',	'-79.4163'),
+(5,	'Brasil',	'Rio de Janeiro',	'-22.9028',	'-43.2075'),
+(6,	'Thailand',	'Phuket',	'7.8906',	'98.3981'),
+(7,	'Australia',	'Sydney',	'-33.8679',	'151.2073'),
+(8,	'South Africa',	'Cape Town',	'-33.9258',	'18.4232'),
+(9,	'Chili',	'Antofagasta',	'-24.2739',	'-69.4097');
 
---
--- Structure de la table `item`
---
-
-CREATE TABLE `item` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `item`
---
-
-INSERT INTO `item` (`id`, `title`) VALUES
-(1, 'Stuff'),
-(2, 'Doodads');
-
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `item`
---
-ALTER TABLE `item`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- 2023-05-11 12:21:00
