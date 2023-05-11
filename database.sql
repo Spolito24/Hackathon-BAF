1e66139326617fe7b64905c9e40d6e9d6ddfb13f
Spolito24 <[baptisterenier69@gmail.com](mailto:baptisterenier69@gmail.com)>
2023-05-11 16:43:26 +0200

    finish index

--- database.sql
+++ database.sql
@@ -16,17 +16,18 @@ CREATE TABLE `destination` (
   `id` int NOT NULL AUTO_INCREMENT,
   `country` varchar(150) DEFAULT NULL,
   `city` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
   `lat` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `long` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `destination` (`id`, `country`, `city`, `lat`, `long`) VALUES
-(1, 'Groenland', 'Narsarmijit', '60.005417', '-44.666359'),
-(2, 'Botswana', 'Ghanzi', '-21.69785', '21.64581'),
-(3, 'Somalie', 'Hobyo', '5.351592', '48.524975'),
-(4, 'Philippine', 'Palawan', '11.350636', '119.461095'),
-(5, 'Bresil', 'Missão Tunuí', '-68.14892', '1.4077399');
+(1, 'Groenland', '\assets\images\greenland-6948495_1280.jpg', 'Narsarmijit', '60.005417', '-44.666359'),
+(2, 'Botswana', '\assets\images\bitobolo-gd78c09cab_1280.jpg', 'Ghanzi', '-21.69785', '21.64581'),
+(3, 'Somalie', '\assets\images\hobyo-6758858.jpg', 'Hobyo', '5.351592', '48.524975'),
+(4, 'Philippine', 'public\assets\images\phillipines-14210287.jpg', 'Palawan', '11.350636', '119.461095'),
+(5, 'Bresil', 'public\assets\images\bresil-9214062.jpg', 'Missão Tunuí', '-68.14892', '1.4077399');
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
